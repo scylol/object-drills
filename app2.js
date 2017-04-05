4dcf7583b116a4aa3b9485236bb392ed5c7f1d0a
Scylol <djd3dobbin@gmail.com>
2017-04-05 14:51:31 -0400

    for in loop

--- app2.js
+++ app2.js
@@ -8,3 +8,17 @@ const loaf = {
 
 console.log(`flour: ${loaf.flour}\nwater: ${loaf.water}`);
 console.log("hydration level is " +loaf.hydration());
+
+
+const myObject = {
+	foo: "1",
+	bar: "2",
+	fum: "3",
+	quux: "4",
+	spam: "5"
+}
+
+
+for(d in myObject) {
+	console.log(myObject[d]);
+}
