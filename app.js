5636532ea84ad739322e4cb4ed242d231233eeed
Scylol <djd3dobbin@gmail.com>
2017-04-05 11:44:51 -0400

    self reference

--- app.js
+++ app.js
@@ -16,3 +16,19 @@ function updateObject(obj){
 	obj.bizz = 'bizz';
 	obj.bang = 'bang';
 }
+
+
+function personMaker() {
+	var person = {
+	firstName: 'Paul',
+	lastName: 'Jones',
+	// replace `null` with a function that uses self reference to return
+	// full name
+	fullName: function(){
+	return `${this.firstName} ${this.lastName}`
+	} 
+
+	};
+	return person;
+}
+
